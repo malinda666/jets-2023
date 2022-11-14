ace67b708fdab8ac25e9fe45fa9214869cd72b0d
Malinda Anupama <55244908+malinda666@users.noreply.github.com>
2022-11-14 19:58:27 +0530

    explore open close mechanism

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,13 +1,18 @@
 import { FC, useRef, useEffect, useState, MutableRefObject } from 'react'
 import Link from 'next/link'
+import gsap from 'gsap'
 
 import { ExploreCallToAction, ExplorePopup } from '../shared'
 
 import useOnClickOutside from '@/hooks/useOutsideClick'
+import { textReveal } from '@/utils/animations'
 
 const Header: FC = () => {
   const exploreButtonRef = useRef() as MutableRefObject<HTMLDivElement>
+  const exploreRef = useRef() as MutableRefObject<HTMLDivElement>
   const exploreWrapperRef = useRef() as MutableRefObject<HTMLDivElement>
+
+  const exploreTl = useRef() as any
 
   const [isExploreOpen, setExploreOpen] = useState<boolean>(false)
 
@@ -16,12 +21,65 @@ const Header: FC = () => {
   useOnClickOutside(exploreWrapperRef, isExploreOpen ? toggleExploreOpen : null)
 
   useEffect(() => {
-    animateExplore(isExploreOpen ? 'open' : 'close')
-  }, [isExploreOpen])
+    const title1 = gsap.utils.selector(exploreWrapperRef.current)('.explore__title > span')
+    const title2 = gsap.utils.selector(exploreWrapperRef.current)(
+      '.explore__title > .explore__title--inner > span'
+    )
+    const exploreWrap = exploreWrapperRef.current
+    gsap.set(exploreWrap, { scaleY: 0 })
+    gsap.set(exploreRef.current, { visibility: 'hidden' })
+    gsap.set([title1, title2], { y: '100%', opacity: 0 })
 
-  const animateExplore = (dir: string) => {
-    console.log(dir)
+    exploreTl.current = gsap
+      .timeline({
+        paused: true,
+      })
+
+      .to(
+        [exploreWrap],
+        {
+          scaleY: 1,
+          duration: 1,
+          ease: 'power3.out',
+        },
+        0
+      )
+      .to(
+        [title1, title2],
+        {
+          duration: 1,
+          y: '0%',
+          opacity: 1,
+          stagger: {
+            from: 'start',
+            amount: 0.3,
+          },
+          ease: 'power2.inOut',
+        },
+        0.3
+      )
+  }, [])
+
+  const openExplore = () => {
+    exploreTl.current.play()
+    gsap.set(exploreRef.current, { visibility: 'visible' })
+    gsap.set(document.body, { overflow: 'hidden' })
   }
+  const closeExplore = () => {
+    exploreTl.current.reverse()
+    setTimeout(() => {
+      gsap.set(exploreRef.current, { visibility: 'hidden' })
+      gsap.set(document.body, { overflow: 'auto' })
+    }, 2300)
+  }
+
+  useEffect(() => {
+    if (isExploreOpen) {
+      openExplore()
+    } else {
+      closeExplore()
+    }
+  }, [isExploreOpen])
 
   return (
     <>
@@ -44,7 +102,11 @@ const Header: FC = () => {
           />
         </nav>
       </header>
-      <ExplorePopup isOpen={isExploreOpen} exploreWrapperRef={exploreWrapperRef} />
+      <ExplorePopup
+        isOpen={isExploreOpen}
+        exploreRef={exploreRef}
+        exploreWrapperRef={exploreWrapperRef}
+      />
     </>
   )
 }
--- src/components/shared/ExplorePopup.tsx
+++ src/components/shared/ExplorePopup.tsx
@@ -1,32 +1,34 @@
-import { FC, MutableRefObject, Ref, useRef } from 'react'
+import { FC, Ref } from 'react'
 
 interface ExplorePopupProps {
   isOpen: boolean
+  exploreRef: Ref<HTMLDivElement>
   exploreWrapperRef: Ref<HTMLDivElement>
 }
 
-const ExplorePopup: FC<ExplorePopupProps> = ({ isOpen, exploreWrapperRef }) => {
+const ExplorePopup: FC<ExplorePopupProps> = ({ isOpen, exploreRef, exploreWrapperRef }) => {
   return (
-    <>
-      {isOpen ? (
-        <div className='explore'>
-          <div className='explore__wrapper' ref={exploreWrapperRef}>
-            <div className='explore__wrapper--inner'>
-              <span className='explore__title--line-inner'>
-                Explore <br />
-                <span className='alt'>Japan</span>
-              </span>
-            </div>
-            <div className='divider__vertical' />
-            <div className='explore__wrapper--inner'>
-              <span className='explore__title--line-inner'>
-                Explore <br /> <span className='alt'>Sri Lanka</span>
-              </span>
+    <div className={['explore', ''].join(' ')} ref={exploreRef}>
+      <div className='explore__wrapper' ref={exploreWrapperRef}>
+        <div className='explore__wrapper--inner'>
+          <div className='explore__title'>
+            <span>Explore</span>
+            <div className='explore__title--inner'>
+              <span>Japan</span>
             </div>
           </div>
         </div>
-      ) : null}
-    </>
+        <div className='divider__vertical' />
+        <div className='explore__wrapper--inner'>
+          <div className='explore__title'>
+            <span>Explore</span>
+            <div className='explore__title--inner'>
+              <span>Sri Lanka</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
